dfe84d3196336817a8388b9c1029403c5125a936
srs-bsns <forfrdm@gmail.com>
2020-09-15 23:57:44 -0400

    Update Forge and mappings + minor fixes
    - Get mappings from Forge repo again.
    - Explicitly declare a main source set refmap due to an issue with the
      latest SpongeGradle snapshot not properly applying the defaults.
    - Migrated mods.toml to LF-only line endings as the CR character shows
      up in in-game GUIs.
    - Removed unused import in BlockConfigMap.
    
    Signed-off-by: srs-bsns <forfrdm@gmail.com>

--- src/main/java/srscode/fallthru/BlockConfigMap.java
+++ src/main/java/srscode/fallthru/BlockConfigMap.java
@@ -38,7 +38,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
